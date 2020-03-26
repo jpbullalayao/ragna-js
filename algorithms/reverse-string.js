40e9ed6539708e8dcfd4b9317e616db7fd60c9b3
Jourdan Bul-lalayao <jpbullalayao@gmail.com>
2020-03-25 21:37:01 -0700

    Add reverseString algorithm

---
+++ algorithms/reverse-string.js
@@ -0,0 +1,23 @@
+/**
+ * reverseString
+ * Given a string s, this returns a reversed version of s
+ *
+ * @param {string} s
+ */
+const reverseString = (s) => {
+  let firstHalf = '';
+  let secondHalf = '';
+
+  for (let i = 0; i < Math.floor(s.length / 2); i++) {
+    const secondChar = s[s.length - i - 1];
+    firstHalf = firstHalf + secondChar; // c
+    secondHalf = s[i] + secondHalf; // a
+  }
+  
+  // s is an odd length, so tack on the middle character
+  if (s.length % 2 !== 0) {
+    firstHalf = firstHalf + s[Math.floor(s.length / 2)]
+  }
+
+  return firstHalf + secondHalf;
+}
