0c54bec6f25498162e94054888c151f6292b6a1b
Jourdan Bul-lalayao <jpbullalayao@gmail.com>
2020-03-25 21:58:02 -0700

    Add hasUniqueChars algorithm

---
+++ algorithms/has-unique-chars.js
@@ -0,0 +1,24 @@
+/**
+ * hasUniqueChars
+ * Given a string 's', this function determines if all the characters in this
+ * string are unique (i.e. no duplicate characters).
+ * 
+ * @param {string} s
+ */
+const hasUniqueChars = (s) => {
+  if (!s || s.length === 0) {
+    return false;
+  }
+
+  const dict = {};
+
+  for (let char of s) {
+    if (dict[char]) {
+      return false;
+    }
+
+    dict[char] = 1;
+  }
+
+  return true;
+}
