f5c4b437ed422481573939ab5fbac9ef6710e936
Jourdan Bul-lalayao <jpbullalayao@gmail.com>
2020-03-25 21:49:16 -0700

    Add isPalindrome algorithm

---
+++ algorithms/is-palindrome.js
@@ -0,0 +1,15 @@
+/**
+ * isPalindrome
+ * Takes in a string 's' and determines if it is a palindrome.
+ *
+ * @param {string} s
+ */
+const isPalindrome = (s) => {
+  for (let i = 0; i < Math.floor(s.length / 2); i++) {
+    if (s[i] !== s[s.length - 1 - i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
